5e7ff2866f98e4bfb9f4c90240019b0d0f9ccdba
Harshit Kushwah <harshitkushwah@Administrators-MacBook-Pro.local>
2024-08-14 00:25:38 +0530

    initial setup

---
+++ myLib123/myLib123.h
@@ -0,0 +1,18 @@
+//
+//  myLib123.h
+//  myLib123
+//
+//  Created by Harshit Kushwah on 14/08/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for myLib123.
+FOUNDATION_EXPORT double myLib123VersionNumber;
+
+//! Project version string for myLib123.
+FOUNDATION_EXPORT const unsigned char myLib123VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <myLib123/PublicHeader.h>
+
+
